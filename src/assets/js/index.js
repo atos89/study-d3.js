31468ed7d2cb05d81e15600242d9f71c77d5833b
atos89 <atos990@gmail.com>
2017-03-02 01:35:15 +0900

    関数の戻り値を引数に与えることもできる

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -24,7 +24,11 @@
         // }
         // d3.selectAll('p').style('color', 'white');
 
-        d3.select('body').style('background-color', 'black');
+        // d3.select('body').style('background-color', 'black');
+
+        d3.selectAll('p').style('color', function () {
+            return 'hsl(' + Math.random() * 360 + ', 100%, 50%)';
+        });
     };
 
     fn.init();
