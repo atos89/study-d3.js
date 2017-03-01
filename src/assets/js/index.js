4bd818ed14a6945a8cfa1bc110c48b41c178c334
atos89 <atos990@gmail.com>
2017-03-02 01:46:29 +0900

    dは常にundefined、iは常に0がセットされていた。引数が何なのかは不明

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -26,9 +26,19 @@
 
         // d3.select('body').style('background-color', 'black');
 
-        d3.selectAll('p').style('color', function () {
-            return 'hsl(' + Math.random() * 360 + ', 100%, 50%)';
+        // d3.selectAll('p').style('color', function () {
+        //     return 'hsl(' + Math.random() * 360 + ', 100%, 50%)';
+        // });
+
+        d3.selectAll('p').style('color', function (d, i) {
+            console.log(d);
+            console.log(i);
+            return i % 2 ? '#fff' : '#eee';
         });
+
+        // d3.selectAll('p')
+        //     .data([4, 8, 15, 16, 23, 42])
+        //     .style('font-size', function (d))
     };
 
     fn.init();
