eb82ea0f667c2a310919ab4f6259b92ad7ef7b17
atos89 <atos990@gmail.com>
2017-03-02 01:59:22 +0900

    ループを書かずとも配列をループしたのと同じ結果になった

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -30,15 +30,18 @@
         //     return 'hsl(' + Math.random() * 360 + ', 100%, 50%)';
         // });
 
-        d3.selectAll('p').style('color', function (d, i) {
-            console.log(d);
-            console.log(i);
-            return i % 2 ? '#fff' : '#eee';
-        });
+        // d3.selectAll('p').style('color', function (d, i) {
+        //     console.log(d);
+        //     console.log(i);
+        //     return i % 2 ? '#fff' : '#eee';
+        // });
 
-        // d3.selectAll('p')
-        //     .data([4, 8, 15, 16, 23, 42])
-        //     .style('font-size', function (d))
+        d3.selectAll('p')
+            .data([4, 8, 15, 16, 23, 42])
+            .style('font-size', function (d) {
+                console.log(d);
+                return d + 'px';
+            });
     };
 
     fn.init();
